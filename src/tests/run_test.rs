e3ce258e6481f0ebee9ea3ca6d5263c2a7111b17
meris <meris.bahti@gmail.com>
2024-07-31 22:08:39 +0200

    woops almost forgot the tests

---
+++ src/tests/run_test.rs
@@ -0,0 +1,83 @@
+#[test]
+fn many_tests() {
+    use crate::vm::jit_run;
+    assert_eq!(
+        jit_run(
+            "
+        (define a '(1 2 3 4))
+        (apply + a)            
+        "
+            .to_string(),
+        ),
+        Ok(crate::expr::Expr::Num(10.0))
+    );
+
+    assert_eq!(
+        jit_run(
+            "
+        (define (fn a b) (+ a b))
+        (define a '(1 2))
+        (apply fn a)            
+        "
+            .to_string(),
+        ),
+        Ok(crate::expr::Expr::Num(3.0))
+    );
+
+    assert_eq!(
+        jit_run(
+            "
+        (define someval 10)
+        (define (fn a b) (+ a b someval))
+        (define a '(1 2))
+        (apply fn a)            
+        "
+            .to_string(),
+        ),
+        Ok(crate::expr::Expr::Num(13.0))
+    );
+
+    assert_eq!(
+        jit_run(
+            "
+        (define someval 10)
+        (define a '(1 2))
+        (apply (lambda (a b) (+ a b someval)) a)            
+        "
+            .to_string(),
+        ),
+        Ok(crate::expr::Expr::Num(13.0))
+    );
+
+    assert_eq!(
+        jit_run(
+            "
+        (define someval 10)
+        (define a '(1 2))
+        (apply (lambda (a b) (+ a b someval)) a)            
+        "
+            .to_string(),
+        ),
+        Ok(crate::expr::Expr::Num(13.0))
+    );
+
+    assert_eq!(
+        jit_run(
+            "
+        (apply (lambda (a b) b) '(0 15))            
+        "
+            .to_string(),
+        ),
+        Ok(crate::expr::Expr::Num(15.0))
+    );
+
+    assert_eq!(
+        jit_run(
+            "
+        (apply (lambda (a b) a) '(0 15))            
+        "
+            .to_string(),
+        ),
+        Ok(crate::expr::Expr::Num(0.0))
+    );
+}
